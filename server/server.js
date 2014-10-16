21c0ab9c4fe1beb333bb9a87e6ac68a42d4f7be8
Lisandro <lisandrofalconi@gmail.com>
2014-10-16 19:18:42 -0300

    add mail function

---
+++ server/server.js
@@ -0,0 +1,10 @@
+process.env.MAIL_URL="smtp://arqavanzada.alerta%40gmail.com:arduino123@smtp.gmail.com:465/"; 
+ /**
+Email.send({
+  from: "arqavanzada.alerta@gmail.com",
+  to: "lisandrofalconi@gmail.com",
+  subject: "Meteor Can Send Emails via Gmail",
+  text: "Its pretty easy to send emails via gmail."
+});
+
+*/
